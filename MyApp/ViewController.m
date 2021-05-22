dd581b8e6786a8d4ad887c43ebb6d359ab6f42d4
KcsjMac <1154180808@qq.com>
2021-05-22 15:42:21 +0800

    study

--- MyApp/ViewController.m
+++ MyApp/ViewController.m
@@ -13,10 +13,17 @@
 @interface ViewController ()<FlutterStreamHandler>
 
 @property (nonatomic,strong) FlutterViewController *flutterVC;
-
+@property (nonatomic,strong) NSMutableArray *dataArr;
 @end
 
 @implementation ViewController
+
+- (NSMutableArray *)dataArr{
+    if (!_dataArr) {
+        _dataArr = [[NSMutableArray alloc]initWithArray:@[@"1",@"12",@"45",@"14",@"54",@"75",@"85",@"19",@"62",@"17",@"98"]];
+    }
+    return _dataArr;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -24,6 +31,9 @@
     self.navigationItem.title = @"原生首页";
     NSArray *dataArr = @[@"flutter列表",@"flutter登录页面",@"flutter练习"];
     [self createButton:dataArr.count withTitle:dataArr];
+    [self quickSort];
+    [self selectSort];
+    [self bubblingSort];
 }
 
 - (CGFloat)getStatusHeight{
@@ -104,5 +114,85 @@
     NSLog(@"%@", arguments);
     return nil;
 }
+
+- (void)quickSort{
+    NSMutableArray *arr = [self.dataArr mutableCopy];
+    [self quickSort:arr start:0 end:arr.count-1];
+    NSLog(@"快速排序法排序后的数组: %@",arr);
+}
+
+//快速排序
+- (void)quickSort:(NSMutableArray *)arr start:(NSInteger)start end:(NSInteger)end {
+    //如果数组长度为0或1时返回
+    if (start >= end) {
+        return;
+    }
+    NSInteger low = start;
+    NSInteger high = end;
+    //记录比较基准数
+    int stand = [arr[start] intValue];
+    while (low < high) {
+        /**** 首先从右边j开始查找比基准数小的值 ***/
+        while (low < high && stand <= [arr[high] intValue]) { //如果比基准数大，继续查找
+            high--;
+        }
+        if (low == high) {
+            break;
+        }
+        //如果比基准数小，则将查找到的小值调换到i的位置(先赋值,然后再low+1)
+        arr[low++] = arr[high];
+        
+        /**** 当在右边查找到一个比基准数小的值时，就从i开始往后找比基准数大的值 ***/
+        while (low < high && stand >= [arr[low] intValue]) {//如果比基准数小，继续查找
+            low++;
+        }
+        if (low == high) {
+            break;
+        }
+        //如果比基准数大，则将查找到的大值调换到j的位置(先赋值,然后再high-1)
+        arr[high--] = arr[low];
+    }
+    //把标准数赋值给低所在的位置
+    arr[low] = @(stand);
+    
+    /**** 递归排序 ***/
+    //排序基准数左边的
+    [self quickSort:arr start:start end:low];
+    //排序基准数右边的
+    [self quickSort:arr start:low+1 end:end];
+}
+
+//选择排序法
+- (void)selectSort{
+    NSString *temp;
+    NSMutableArray *arr = [self.dataArr mutableCopy];
+    for (int i = 0; i<arr.count-1; i++) {
+        for (int j = i+1; j<arr.count; j++) {
+            if ([arr[i] intValue] > [arr[j] intValue]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    NSLog(@"选择排序法排序后的数组: %@",arr);
+}
+
+//冒泡排序法
+- (void)bubblingSort{
+    NSString *temp;
+    NSMutableArray *arr = [self.dataArr mutableCopy];
+    for (int i = 0; i<arr.count-1; i++) {
+        for (int j = 0; j<arr.count-1-i; j++) {
+            if ([arr[j] intValue] > [arr[j+1] intValue]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    NSLog(@"冒泡排序法排序后的数组: %@",arr);
+}
+
 
 @end
